90e9df575b515ee27151302bff86d3accf48d03b
olivius <b.oliviu@yahoo.com>
2015-07-06 13:59:57 +0300

    Register and login

--- Gamers/script.js
+++ Gamers/script.js
@@ -1,27 +1,45 @@
 var app = angular.module('firstPageModule', []);
+
 app.controller('firstPageController', ['$scope', function ($scope) {
     $scope.email = " ";
     $scope.password = " ";
     $scope.repassword = " ";
     $scope.registerUser = function () {
-        debugger;
-        alert("");
+        alert(".");
         jNorthPole.BASE_URL = 'https://json.northpole.ro/';
         jsonObject = {
             "api_key": $scope.email,
             "secret": $scope.password
         }
-        jNorthPole.getStorage(jsonObject, registerResponseHandler);}
-    
-    
+        responseHandler = function (data) {console.log(data);
+                                          };
+        jNorthPole.postUser(jsonObject, responseHandler);
+    }
 }]);
-                                       
-registerResponseHandler = function (data) {
+
+responseHandler = function (data) { alert("Success");
+        };    
+
+var app = angular.module('firstPageModule', []);
+
+app.controller('firstLoginController', ['$scope', function ($scope) {
+    $scope.email = " ";
+    $scope.password = " ";
+    $scope.loginUser = function () {
+        jNorthPole.BASE_URL = 'https://json.northpole.ro/';
+        jsonObject = {
+            "api_key": $scope.email,
+            "secret": $scope.password
+        }
+        responseHandler = function (data) {console.log(data);
+        };
+        jNorthPole.getStorage(jsonObject, responseHandler);}
+}]);
+
+responseHandler = function (data) {
             alert("Success");
-                                       };
-		
-		
-		
+                                       };			
+/*		
 function validarenume() {
     var x = document.forms["form"]["email"].value;
     if (x == null || x == "") {                                        //verifica daca este un email scris
@@ -44,38 +62,31 @@ function validarenume() {
         return false;
     }
 }
+*/
 
 //cookie pt login
 
-
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + "; " + expires;
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
 }
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for (var i=0; i<ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
 }
 
-function checkCookie() {
-    var user=getCookie("username");
-    if (user != "") {
-        alert("Welcome again " + user);
-    } else {
-       user = prompt("Please enter your name:","");
-       if (user != "" && user != null) {
-           setCookie("username", user, 30);
-       }
-    }
+function eraseCookie(name) {
+	createCookie(name,"",-1);
 }
