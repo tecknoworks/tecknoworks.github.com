4d1a46b8e5f65345b4bb0cd152d6914f17f36876
olivius <b.oliviu@yahoo.com>
2015-07-10 14:22:05 +0300

    organizare... to do: after register, login

--- Gamers/script.js
+++ Gamers/script.js
@@ -1,53 +1,55 @@
 var userId = "userID";
 
-var app = angular.module('firstPageModule',[]);
+var app = angular.module('firstPageModule', []);
 
-app.controller('firstPageController', function ($scope){
+app.controller('firstPageController', function ($scope) {
+    $scope.init = function () {
+        var isUserIn = localStorage.getItem(userId);
+        if (isUserIn && isUserIn.length > 0) 
+        {
+            location.href = 'main.html';
+        }
+    };
+    responseHandler = function (data) {
+        if (data[0].id != undefined)
+        {
+        localStorage.setItem(userId,data[0].id);
+        location.href = 'main.html';
+        } 
+    };
+});
+
+app.controller('registerController', function ($scope) {
     $scope.username = "";
     $scope.password = "";
     $scope.repassword = "";
     $scope.registerUser = function () {
         jNorthPole.BASE_URL = 'https://json.northpole.ro/';
-        responseHandler = function (data) {console.log(data);};
         jNorthPole.createUser($scope.username, $scope.password, responseHandler);
-    }
-    
-    $scope.init = function(){
-        var isUserIn = localStorage.getItem(userId); 
-        if(isUserIn && isUserIn.length > 0)
-        {
-            location.href = 'main.html';
-        };
+        responseHandler = function (data) {console.log(data); };
     };
+});
+
+app.controller('loginController', function ($scope) {
     $scope.usernameLogin = "";
     $scope.passwordLogin = "";
     $scope.loginUser = function () {
         jNorthPole.BASE_URL = 'https://json.northpole.ro/';
-        jsonObj = { 
-            "api_key": $scope.usernameLogin,    
+        jsonObj = {
+            "api_key": $scope.usernameLogin,
             "secret": $scope.passwordLogin
         };
         jNorthPole.getStorage(jsonObj, responseHandler);
-        
-}
-    
-    });
-
-responseHandler = function (data){
-    if(data[0].id != undefined)
-    {
-        localStorage.setItem(userId,data[0].id);
-        location.href = 'main.html';
     };
-}
-
-app.controller('secondPageController',function ($scope){
-    $scope.logOut=function(){
-        var isUserIn= localStorage.getItem(userId);
-        if (isUserIn && isUserIn.length > 0) {
-        localStorage.removeItem(userId);
-        };
-    }
-
 });
+
+app.controller('secondPageController', function ($scope) {
+    $scope.logOut = function () {
+        var isUserIn = localStorage.getItem(userId);
+        if (isUserIn && isUserIn.length > 0) {
+            localStorage.removeItem(userId);
+        }
+    };
+});
+
 
